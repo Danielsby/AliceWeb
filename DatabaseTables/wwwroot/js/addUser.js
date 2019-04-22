f834c90cf2e83c2bf13fb44ef96aa8e586dad93a
ahkw <danielsby@outlook.com>
2019-04-23 01:26:56 +0200

    Added button-template for register

---
+++ DatabaseTables/wwwroot/js/addUser.js
@@ -0,0 +1,19 @@
+﻿/**
+ * Put username and password,
+ * inside the user table with Ajax. 
+ */
+
+function requestToBackend() {
+    var xhttp = new XMLHttpRequest();
+
+    /* Receive and treat the response. */ 
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status = 200) {
+            document.getElementById("result").innerHTML = this.responseText;
+        }
+    };
+
+    /* Send request. */ 
+    xhttp.open("GET", "api/commands/{$id}", true);
+    xhttp.send();
+}
