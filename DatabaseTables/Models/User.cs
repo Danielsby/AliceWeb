dbd35c7d558d71e5c97110d9a41c64a9edac4d66
ahkw <danielsby@outlook.com>
2019-04-25 15:00:31 +0200

    Added basics for authentication with two roles.

--- DatabaseTables/Models/User.cs
+++ DatabaseTables/Models/User.cs
@@ -7,8 +7,12 @@ namespace DatabaseTables.Models
 {
     public class User
     {
-        public int ID { get; set; }
-        public int Password { get; set; }
-        public string Name { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set;  }
     }
 }
---
+++ DatabaseTables/Services/IUserService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using DatabaseTables.Services;
+using DatabaseTables.Models;
+using DatabaseTables.Helpers;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+
+namespace DatabaseTables.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+
+    public class UserService : IUserService
+    {
+        // Users hardcoded for simplicity, store in a db with hashed password in production applicatiaons. 
+        // TODO: Put this inside the database. 
+        private List<User> _user = new List<User>
+        {
+            new User { Id = 1, FirstName = "admin", LastName = "User", Username = "admin", Password = "admin", Role = Role.Admin},
+            new User { Id = 1, FirstName = "Normal", LastName = "User", Username = "user", Password = "user", Role = Role.User }
+        };
+
+        private readonly AppSettings _appSettings;
+
+        public UserService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            var user = _user.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            // Return null if user not found.
+            if (user == null)
+                return null;
+
+            // Authentication successful so generate jwt token.
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            // Remove password before returning. 
+            user.Password = null;
+
+            return user;            
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _user.Select(x =>
+            {
+                x.Password = null;
+                return x;
+            });
+        }
+
+        public User GetById(int id)
+        {
+            var user = _user.FirstOrDefault(x => x.Id == id);
+
+            // Return user without password.
+            if (user != null)
+            {
+                user.Password = null;
+            }
+
+            return user;
+        }
+    }
+}
