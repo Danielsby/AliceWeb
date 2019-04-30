27be63784bc0d4286baa48f087697ef3916bfb04
ahkw <danielsby@outlook.com>
2019-05-01 00:36:08 +0200

    Commented out a few comments.

--- DatabaseTables/wwwroot/js/logIn.js
+++ DatabaseTables/wwwroot/js/logIn.js
@@ -4,11 +4,11 @@
  */
 function testFunction() {
     var xhttp = new XMLHttpRequest();
-    console.log("User: ");
+    // console.log("User: "); <-- Test-function. 
     console.log(username_id);
     console.log("tries to log in");
     
-    // Receive and treat the response. 
+    // Receive and treat the response from the backend. 
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
@@ -16,6 +16,8 @@ function testFunction() {
     };
 
     // Send request. 
-    xhttp.open("GET", "http://localhost:5000/api/commands/1", true);
+    // xhttp.open("GET", "http://localhost:5000/api/commands/1", true);
+
+    // Redirect to the front-page. <-- Happens in the backend?
     xhttp.send();
 }
