986d7d879c09aefa8bacd116a2bc721612072548
ahkw <danielsby@outlook.com>
2019-05-18 23:00:20 +0200

    Attempt to fix reset. Added custom messages to detector and temperature.

--- DatabaseTables/Controllers/ResetController.cs
+++ DatabaseTables/Controllers/ResetController.cs
@@ -2,32 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using DatabaseTables.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DatabaseTables.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class LogController : ControllerBase
+    public class ResetController : ControllerBase
     {
         private readonly DataContext _context;
 
-        public LogController(DataContext context) => _context = context;
+        public ResetController(DataContext context) => _context = context;
 
-        [HttpGet]
-        public ActionResult<IEnumerable<Log>> GetData()
-        {
-            return _context.Clicks;
-        }
-
-        // PUT: api/commands/n
         [HttpPut("{id}")]
-        public ActionResult<Log> AddClick(int id, Log command)
+        public ActionResult<Log> ResetClick(int id, Log command)
         {
             var row = _context.Clicks.Find(1);
 
-            row.Clicks += id;
+            row.Clicks = id;
 
             _context.SaveChanges();
 
